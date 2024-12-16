2504e664c342029ebc87bbda287f81889bfdd0c0
LawrenceLee1999 <lawrencelee1999@hotmail.co.uk>
2024-12-16 15:53:03 +0000

    feat(user): add user profile update endpoint
    - Added route for user profile updates: PUT /user/update-user/:id
    - Implemented logic to update user profile fields
    - Connected endpoint with database for user updates

---
+++ controllers/user-controller.js
@@ -0,0 +1,86 @@
+import bcrypt from "bcrypt";
+import pg from "pg";
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const {
+    email,
+    password,
+    name,
+    specialisation,
+    contact_info,
+    team,
+    experience,
+    sport,
+    gender,
+    position,
+    date_of_birth,
+  } = req.body;
+
+  try {
+    const userResult = await pool.query("SELECT * FROM users where id = $1", [
+      id,
+    ]);
+    if (userResult.rows.length === 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const user = userResult.rows[0];
+
+    const updatedFields = [];
+    const updateValues = [];
+
+    if (email) {
+      updatedFields.push("email = $1");
+      updateValues.push(email);
+    }
+    if (name) {
+      updatedFields.push("name = $2");
+      updateValues.push(name);
+    }
+    if (password) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      updatedFields.push("password = $3");
+      updateValues.push(hashedPassword);
+    }
+    if (updatedFields.length > 0) {
+      await pool.query(
+        `UPDATE users SET ${updatedFields.join(", ")} WHERE id = $4`,
+        [...updateValues, id]
+      );
+    }
+    switch (user.role) {
+      case "clinician":
+        await pool.query(
+          "UPDATE clinicians SET specialisation = $1, contact_info = $2 WHERE user_id = $3",
+          [specialisation, contact_info, id]
+        );
+        break;
+      case "coach":
+        await pool.query(
+          "UPDATE coaches SET team = $1, experience = $2 WHERE user_id = $3",
+          [team, experience, id]
+        );
+        break;
+      case "athlete":
+        await pool.query(
+          "UPDATE athletes SET sport = $1, gender = $2, position = $3, date_of_birth = $4 WHERE user_id = $5",
+          [sport, gender, position, date_of_birth, id]
+        );
+        break;
+      default:
+        return res.status(400).json({ message: "Invalid role specified" });
+    }
+
+    res.status(200).json({ message: "Update user successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
