07259f6ad0a2f76cbb56c635b6dd3302803684d9
LawrenceLee1999 <lawrencelee1999@hotmail.co.uk>
2025-01-08 12:05:42 +0000

    Add get deviations from baseline endpoint

--- routes/score-routes.js
+++ routes/score-routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createBaselineScore,
   createTestScore,
+  getDeviations,
 } from "../controllers/score-controller.js";
 import { authenticate } from "../middlewares/authenticate.js";
 
@@ -10,5 +11,7 @@ const router = express.Router();
 router.post("/baseline-scores", authenticate, createBaselineScore);
 
 router.post("/test-scores", authenticate, createTestScore);
+
+router.get("/deviations", authenticate, getDeviations);
 
 export default router;
