d25e161bf45311fb572b05ec611882edae394399
Lawrence Lee <125479060+LawrenceLee1999@users.noreply.github.com>
2025-02-19 00:20:34 +0000

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,12 @@ const port = process.env.PORT || 3000;
 const corsOptions = {
   origin: "https://scotia-sense-frontend.onrender.com",
   credentials: true,
+  methods: "GET, POST, PUT, DELETE, OPTIONS",
+  allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization",
 };
 
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
