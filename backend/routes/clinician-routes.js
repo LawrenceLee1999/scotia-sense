61d42e9df44a6e1f08ba4ed9a6e681f4d12ab47b
LawrenceLee1999 <lawrencelee1999@hotmail.co.uk>
2025-06-24 12:10:31 +0100

    Remove creating invites from clinician endpoints since invites are generalised

--- backend/routes/clinician-routes.js
+++ backend/routes/clinician-routes.js
@@ -1,14 +1,9 @@
 import express from "express";
 import { authenticate } from "../middlewares/authenticate.js";
-import {
-  createInvite,
-  getAssignedAthletes,
-} from "../controllers/clinician-controller.js";
+import { getAssignedAthletes } from "../controllers/clinician-controller.js";
 
 const router = express.Router();
 
 router.get("/athletes", authenticate, getAssignedAthletes);
-
-router.post("/invite", authenticate, createInvite);
 
 export default router;
