61d86b6a5a0330b1e844a48bcd72579efbd5dce8
LawrenceLee1999 <lawrencelee1999@hotmail.co.uk>
2025-07-28 14:34:02 +0100

    Change to none for cookies

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -213,7 +213,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      sameSite: "None",
       maxAge: 60 * 60 * 1000,
     });
 
